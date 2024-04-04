d528e0bdbe9673f19bdda928e348f177fb8e180c
JuanRios1011 <juandavidrios1624@gmail.com>
2024-04-03 21:07:50 -0500

    Ejercicios Juan

---
+++ Ejercicio3/ejercicio3.js
@@ -0,0 +1,14 @@
+console.log("Ejercicio 3")
+function res(numero1, numero2){
+    if (numero1 > numero2){
+        return numero1 - numero2
+    }else{
+        return numero2 - numero1
+    }
+    
+}
+console.log(res(10,5))
+console.log(res(20,30))
+    
+
+
