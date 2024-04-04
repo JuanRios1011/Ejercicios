b904f9350a602e52601df80860e9baa5d3af3bde
JuanRios1011 <juandavidrios1624@gmail.com>
2024-04-03 20:46:07 -0500

    Ejercicios Juan

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,2 +1,10 @@
 console.log("Ejercicio 1")
-
+numero = 7
+function par_o_impar(numero){
+    if(numero % 2 == 0){
+        console.log(`el numero ${numero} es par`)
+    } else {
+        console.log(`el numero ${numero} es impar`)
+    }
+}
+par_o_impar(numero)
