d300286a1cbf2ba10597b499cf2fdf4b590e1b59
JuanRios1011 <juandavidrios1624@gmail.com>
2024-04-07 16:02:52 -0500

    Ejercicios Juan

--- Ejercicio5/ejercicio5.js
+++ Ejercicio5/ejercicio5.js
@@ -1,12 +1,12 @@
 console.log("Ejercicio 5")
-
+kilometros=220
+tiempo=4
 function conductor(tiempo, kilometros){
     if (tiempo==4 && kilometros==220){
         console.log("conductor calificado")
     }else{
         console.log("conductor descalificado")
-    }
-    
+    } 
 }
-kilometros=220
-tiempo = 4
+console.log(conductor(tiempo, kilometros))
+
