7b7271f18282406612f86ed1d54a75437f39521d
mfhenao <mf.henao10@ciaf.edu.co>
2024-04-08 09:12:48 -0500

    ejercicio6 OK

---
+++ EJercicio6/ejercicio6.js
@@ -0,0 +1,14 @@
+let numeroArticulos = prompt("Ingrese el número de artículos:");
+let precioArticulo = prompt("Ingrese el precio de cada artículo en euros:");
+
+let cantidadArticulos = parseInt(numeroArticulos);
+let precioUnitario = parseFloat(precioArticulo);
+
+let sinDescuento = cantidadArticulos * precioUnitario;
+if (cantidadArticulos > 10 && precioUnitario > 40) {
+    let descuento = sinDescuento * 0.15;
+    let conDescuento = sinDescuento - descuento;
+    console.log("Cantidad a pagar con descuento del 15%:", conDescuento, "euros");
+} else {
+    console.log("Cantidad a pagar sin descuento:", sinDescuento, "euros");
+}
