59524f2e9114d891d9c596a51b65f6e85191dc35
mfhenao <mf.henao10@ciaf.edu.co>
2024-04-08 07:47:42 -0500

    ejercicio2 OK

---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,21 @@
+let numero = prompt("Ingresa un número:");
+        numero = parseFloat(numero);
+        if (numero > 0) {
+        console.log("Tu número es positivo");
+        } else if (numero < 0) {
+        console.log("Tu número es negativo");
+        } else {
+        console.log("Tu número es igual a 0");
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
