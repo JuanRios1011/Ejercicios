ee3e642cd045d71e19a33263f2de304e63b53160
mfhenao <mf.henao10@ciaf.edu.co>
2024-04-08 23:38:00 -0500

    ejercicio8 OK

---
+++ Ejercicio8/ejercicio8.js
@@ -0,0 +1,12 @@
+console .log("ejercicio 8............................................")
+let edad= prompt("¿ cuantos años tienes ?: ");
+let ingresos= prompt("¿ a cuanto equivalen tus ingresos mensuales en euros? ");
+
+if (edad > 16 && ingresos >= 1000){
+    console.log("Usted debe pagar el impuesto");
+} else {
+
+    console.log("Usted no debe pagar el impuesto")
+}
+
+
